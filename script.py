dae1e9a10b8ad676b06df8d8d12e8f819d6a3f30
patriciacatandi <patriciabcatandi@gmail.com>
2024-10-15 15:53:25 -0300

    testing uv

---
+++ script.py
@@ -0,0 +1,15 @@
+# script.py
+import os
+
+with open('requirements_venv.txt') as f:
+    requirements = f.readlines()
+
+with open('pyproject.toml', 'a') as f:
+    f.write("\ndependencies = [\n")
+    for req in requirements:
+        req = req.strip()
+        # Adiciona ^ após ==
+        if '==' in req:
+            req = req.replace('==', '>=')
+        f.write(f'    "{req}",\n')
+    f.write("]\n")
