17d316e71e5ce919b37a74a58de56f2406c49f6b
patriciacatandi <patriciabcatandi@gmail.com>
2024-12-17 11:10:02 -0300

    changing to cmap.set_under("#FFFFFF00")

--- pipelines/precipitation_model/rionowcast/tasks.py
+++ pipelines/precipitation_model/rionowcast/tasks.py
@@ -388,7 +388,7 @@ def create_image(dataframe: pd.DataFrame, filename: str) -> List:
     values, colors = zip(*filtered_colors)
     vmin, vmax = min(values), max(values)
     cmap = mcolors.LinearSegmentedColormap.from_list("custom_cmap", colors)
-    cmap.set_under("#FFFFFF")
+    cmap.set_under("#FFFFFF00")
     norm = mcolors.Normalize(vmin=vmin, vmax=vmax)
 
     # norm = CustomNormalize(vmin=vmin, vmax=vmax)
